ff22c752616248d25e66a1bfb68ab52a1c8a67d1
Mateus Cruz <emiteze@hotmail.com>
2020-08-03 20:55:33 -0300

    Initial commit with base project structure

---
+++ web/src/main/java/br/com/mtz/cleanarch/web/CleanarchApplication.java
@@ -0,0 +1,13 @@
+package br.com.mtz.cleanarch.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanarchApplication.class, args);
+	}
+
+}
