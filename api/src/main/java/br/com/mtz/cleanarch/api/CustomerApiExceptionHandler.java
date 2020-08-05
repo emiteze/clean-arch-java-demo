c87f47e48ad7458a0e2ab56f1eb0be1f262a7dbe
Mateus Cruz <emiteze@hotmail.com>
2020-08-05 10:12:04 -0300

    Adding exception handler

--- api/src/main/java/br/com/mtz/cleanarch/api/CustomerApiExceptionHandler.java
+++ api/src/main/java/br/com/mtz/cleanarch/api/CustomerApiExceptionHandler.java
@@ -1,4 +1,28 @@
 package br.com.mtz.cleanarch.api;
 
+import br.com.mtz.cleanarch.application.response.ResourceValueResponse;
+import br.com.mtz.cleanarch.domain.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
 public class CustomerApiExceptionHandler {
+
+    public Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    private ResourceValueResponse exceptions(HttpServletRequest request, NotFoundException exception) {
+        this.logger.error(exception.getMessage(), exception);
+        return new ResourceValueResponse(exception.getResourceName(), exception.getId());
+    }
+
 }
